6025843151ff59ffb30b2fa29d5e25a38c638e5c
duzybuzz <wenweng003@gmail.com>
2025-03-25 14:39:53 +0800

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,5 @@ export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users', component: UsersComponent }
+  
 ];
